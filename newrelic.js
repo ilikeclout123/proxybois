e63cacf5669b8584b50632f3e7b508c0d0b1994a
Glitch (webproxy) <none>
2018-06-11 15:20:22 +0000

    📖🐃 Checkpoint ./newrelic.js:861497/22 ./README.md:861497/1503

--- newrelic.js
+++ newrelic.js
@@ -8,7 +8,7 @@ exports.config = {
     /**
      * Array of application names.
      */
-    app_name: ['Node Unblocker'],
+    app_name: ['webproxy'],
     /**
      * Your New Relic license key.
      */
