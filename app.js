f0c49fd6966e78c70e76822482cc1a6b924748e4
Glitch (webproxy) <none>
2018-06-11 15:10:06 +0000

    👾🏔 Checkpoint ./app.js:861497/3902 ./public/index.html:861497/30 ./package.json:861497/83

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-var url=require('url');
-var querystring=require('querystring');
 var express=require('express');
-var unblocker=require('unblocker');
-var Transform=require('stream').Transform;
 var app=express();
 /*
 var google_analytics_id=process.env.GA_ID||null;
 function addGa(html){return google_analytics_id?html.replace("</body>","<script type=\"text/javascript\">var _gaq=[];_gaq.push(['_setAccount','"+google_analytics_id+"']);_gaq.push(['_trackPageview']);(function(){var ga= document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script>\n</body>"):html;}
-function googleAnalyticsMiddleware(data){if(data.contentType=='text/html')data.stream=data.stream.pipe(new Transform({decodeStrings:false,transform:function(chunk,encoding,next){this.push(addGa(chunk.toString()));next();}}));}
+function googleAnalyticsMiddleware(data){if(data.contentType=='text/html')data.stream=data.stream.pipe(new (require('stream').Transform)({decodeStrings:false,transform:function(chunk,encoding,next){this.push(addGa(chunk.toString()));next();}}));}
 */
-var unblockerConfig={prefix:'/~/',responseMiddleware:[]};//googleAnalyticsMiddleware
-app.use(unblocker(unblockerConfig));
-app.use('/', express.static(__dirname+'/public'));
-console
-app.get("/no-js",function(req,res){res.redirect(unblockerConfig.prefix+querystring.parse(url.parse(req.url).query).url);});
+var prefix='/~/';
+app.use(require('unblocker')({prefix:prefix,responseMiddleware:[]}));//googleAnalyticsMiddleware
+app.use('/',express.static(__dirname+'/public'));
+app.get("/no-js",function(req,res){res.redirect(prefix+require('querystring').parse(require('url').parse(req.url).query).url);});
 module.exports=app;
