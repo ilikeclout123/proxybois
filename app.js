6569bcee882479e46b79d90c98f3b378c1576afb
Glitch (webproxy) <none>
2018-06-11 15:52:21 +0000

    🙇🏔 Checkpoint ./app.js:861497/70 ./public/index.html:861497/2

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ function googleAnalyticsMiddleware(data){if(data.contentType=='text/html')data.s
 var prefix='/~/';
 app.use(require('unblocker')({prefix:prefix,responseMiddleware:[]}));//googleAnalyticsMiddleware
 app.use('/',express.static(__dirname+'/public'));
-app.get("/no-js",function(req,res){res.redirect(prefix+require('querystring').parse(url.parse(req.url).query).url);});
+app.get("/no-js",function(req,res){res.redirect(prefix+url.parse(req.url).searchParams.get('url'));});
 app.use(function(req,res,next){res.status(404);res.type('html').send('<meta http-equiv="refresh" content="0;url='+url.resolve(req.url,'/')+'">');});
 module.exports=app;
