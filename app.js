cf0fdcd945daa984e852fe66452c496daf225512
Glitch (webproxy) <none>
2018-06-11 14:59:49 +0000

    🌸🎡 Checkpoint ./app.js:861497/3242 ./public/index.html:861497/30 ./package.json:861497/83

--- app.js
+++ app.js
@@ -6,28 +6,12 @@ var Transform=require('stream').Transform;
 var app=express();
 /*
 var google_analytics_id=process.env.GA_ID||null;
-function addGa(html){
-    if(google_analytics_id){
-        var ga=[
-            "<script type=\"text/javascript\">
-            var _gaq=[];
-            _gaq.push(['_setAccount','"+google_analytics_id+"']);
-            _gaq.push(['_trackPageview']);
-            (function(){
-            var ga= document.createElement('script');ga.type='text/javascript';ga.async=true;
-            ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';
-            var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);
-            })();
-            </script>"
-            ].join("\n");
-        html=html.replace("</body>",ga+"\n\n</body>");
-    }
-    return html;
-}
+function addGa(html){return google_analytics_id?html.replace("</body>","<script type=\"text/javascript\">var _gaq=[];_gaq.push(['_setAccount','"+google_analytics_id+"']);_gaq.push(['_trackPageview']);(function(){var ga= document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script>\n</body>"):html;}
 function googleAnalyticsMiddleware(data){if(data.contentType=='text/html')data.stream=data.stream.pipe(new Transform({decodeStrings:false,transform:function(chunk,encoding,next){this.push(addGa(chunk.toString()));next();}}));}
 */
 var unblockerConfig={prefix:'/~/',responseMiddleware:[]};//googleAnalyticsMiddleware
 app.use(unblocker(unblockerConfig));
 app.use('/', express.static(__dirname+'/public'));
+console
 app.get("/no-js",function(req,res){res.redirect(unblockerConfig.prefix+querystring.parse(url.parse(req.url).query).url);});
 module.exports=app;
