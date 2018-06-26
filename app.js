13fb6c676f7eb45a9e6ab31cc046b86f512ec3a5
Glitch (webproxy) <none>
2018-06-26 15:20:07 +0000

    👏🍮 Checkpoint ./app.js:861497/309

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var url=require('url');
 var express=require('express');
 var app=express();
 var prefix='/~/';
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(require('unblocker')({prefix:prefix,responseMiddleware:[]}));
 app.use('/',express.static(__dirname+'/public'));
 app.get("/-",function(req,res){res.redirect(prefix+url.parse(req.url,true).query.url);});
