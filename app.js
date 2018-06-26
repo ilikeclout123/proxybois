3579c74b61469a88870fce523aeef0bc6c4b6d3a
Glitch (webproxy) <none>
2018-06-26 15:09:23 +0000

    🍵✍️ Checkpoint ./app.js:861497/1024 ./package.json:861497/1176

--- app.js
+++ app.js
@@ -6,4 +6,3 @@ app.use(require('unblocker')({prefix:prefix,responseMiddleware:[]}));
 app.use('/',express.static(__dirname+'/public'));
 app.get("/-",function(req,res){res.redirect(prefix+url.parse(req.url,true).query.url);});
 app.use(function(req,res,next){res.status(404);res.type('html').send('<meta http-equiv="refresh" content="0;url='+url.resolve(req.url,'/')+'">');});
-module.exports=app;
